68fb5646dfd848a06703687d43b294165b77d9e9
Almehaboob <allualmehboob10@gmail.com>
2025-11-02 20:56:03 +0530

    Remove autogenerated OpenAPI client and ignore it in git

--- src/lib/api/models/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ErrorResponse';
-export * from './ExecutiveTokenSchema';
-export * from './GrantType';
-export * from './HTTPValidationError';
-export * from './PlatformType';
-export * from './ValidationError';
-export * from './ValidationErrorLocInner';
