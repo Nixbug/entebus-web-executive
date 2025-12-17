a43315f9577fcd02e1d0a71ed198c47d7bd71fad
Almehaboob <allualmehboob10@gmail.com>
2025-12-17 11:38:45 +0530

    deleted landmark section for screen recording purpose

--- src/routes/landmark/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-</script>
-
-<div class="d-flex">
-  <main class="d-flex flex-grow-1 vh-100 align-items-center justify-content-center p-4">
-    <h1>Welcome to the landmark page.</h1>
-  </main>
-</div>
