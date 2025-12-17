0f3472a52318d404daaf9a59a7c6253319733061
Almehaboob <allualmehboob10@gmail.com>
2025-12-17 12:17:52 +0530

    Initial setup for company dashboard page

--- src/routes/company/+page.svelte
+++ src/routes/company/+page.svelte
@@ -4,5 +4,6 @@
 <div class="d-flex">
 	<main class="d-flex flex-grow-1 vh-100 align-items-center justify-content-center p-4">
 		<h1>Welcome to the company page.</h1>
+		<p>Here you can manage your company information</p>
 	</main>
 </div>
