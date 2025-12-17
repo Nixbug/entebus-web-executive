431742e933da21190034ce7447b9a0d0052bad32
Almehaboob <allualmehboob10@gmail.com>
2025-12-17 12:03:54 +0530

    Working on landmark page.

--- src/routes/landmark/+page.svelte
+++ src/routes/landmark/+page.svelte
@@ -1,4 +1,5 @@
 <div class="d-flex flex-grow-1 vh-100 align-items-center justify-content-center p-4">
 	<h1>Landmark Page</h1>
 	<p>Welcome to the landmark page!</p>
+	<p>This page is currently under construction</p>
 </div>
