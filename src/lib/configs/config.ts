80ae94f7567970505a04bc2de87bc2a6325aefc2
Almehaboob <allualmehboob10@gmail.com>
2026-02-04 10:05:01 +0530

    fix: add type annotation for BASE_URL in config

--- src/lib/configs/config.ts
+++ src/lib/configs/config.ts
@@ -1,3 +1,3 @@
 import { PUBLIC_BASE_URL } from '$env/static/public';
 
-export const BASE_URL = PUBLIC_BASE_URL || 'http://localhost:5173';
+export const BASE_URL: string = PUBLIC_BASE_URL || 'http://localhost:5173';
