4467d2414b5b258ef718afe12cddf3a47eafe608
Almehaboob <allualmehboob10@gmail.com>
2026-02-16 12:57:58 +0530

    Refactor operator role permission tree to streamline structure and enhance clarity

--- src/lib/role-permissions/role-permission-tree.ts
+++ src/lib/role-permissions/role-permission-tree.ts
@@ -124,51 +124,49 @@ export const operatorRolePermissionTree: PermissionNodeData[] = [
     {
         id: "company",
         label: "Company",
-        actions: ["fetch", "update"],
+        actions: ["fetch", "update"]
+    },
+    {
+        id: "bus",
+        label: "Bus",
+        actions: ["fetch", "create", "update", "delete"]
+    },
+    {
+        id: "fare",
+        label: "Fare",
+        actions: ["fetch", "create", "update", "delete"]
+    },
+    {
+        id: "route",
+        label: "Route",
+        actions: ["fetch", "create", "update", "delete"]
+    },
+    {
+        id: "operator",
+        label: "Operator",
+        actions: ["fetch", "create", "update", "delete"],
         children: [
             {
-                id: "bus",
-                label: "Bus",
+                id: "role",
+                label: "Role",
                 actions: ["fetch", "create", "update", "delete"]
             },
             {
-                id: "fare",
-                label: "Fare",
+                id: "token",
+                label: "Token",
                 actions: ["fetch", "create", "update", "delete"]
-            },
+            }
+        ]
+    },
+    {
+        id: "service",
+        label: "Service",
+        actions: ["fetch", "create", "update", "delete"],
+        children: [
             {
-                id: "route",
-                label: "Route",
+                id: "duty",
+                label: "Duty",
                 actions: ["fetch", "create", "update", "delete"]
-            },
-            {
-                id: "operator",
-                label: "Operator",
-                actions: ["fetch", "create", "update", "delete"],
-                children: [
-                    {
-                        id: "role",
-                        label: "Role",
-                        actions: ["fetch", "create", "update", "delete"]
-                    },
-                    {
-                        id: "token",
-                        label: "Token",
-                        actions: ["fetch", "create", "update", "delete"]
-                    }
-                ]
-            },
-            {
-                id: "service",
-                label: "Service",
-                actions: ["fetch", "create", "update", "delete"],
-                children: [
-                    {
-                        id: "duty",
-                        label: "Duty",
-                        actions: ["fetch", "create", "update", "delete"]
-                    }
-                ]
             }
         ]
     }
